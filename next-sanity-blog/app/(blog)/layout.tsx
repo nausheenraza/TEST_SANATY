8cee4b3b0a0097b8edd3fd46f270cc2ae1402f56
nausheenraza <163857169+nausheenraza@users.noreply.github.com>
2025-01-10 04:01:18 +0500

    commit

--- next-sanity-blog/app/(blog)/layout.tsx
+++ next-sanity-blog/app/(blog)/layout.tsx
@@ -9,7 +9,7 @@ import {
 } from "next-sanity";
 import { Inter } from "next/font/google";
 import { draftMode } from "next/headers";
-
+import Link from "next/link";
 import AlertBanner from "./alert-banner";
 import PortableText from "./portable-text";
 
@@ -83,12 +83,12 @@ export default async function RootLayout({
                     Thanks to visit 
                   </h3>
                   <div className="flex flex-col items-center justify-center lg:w-1/2 lg:flex-row lg:pl-4">
-                    <link
+                    <Link
                       href="/"
                       className="mx-3 mb-6 border border-black bg-black py-3 px-12 font-bold text-white transition-colors duration-200 hover:bg-white hover:text-black lg:mb-0 lg:px-8"
                     >
                       BACK TO HOME
-                    </link>
+                    </Link>
                     {/* <a
                       href="/"
                       className="mx-3 font-bold hover:underline"
